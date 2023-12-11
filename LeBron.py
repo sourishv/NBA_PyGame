af1a89ef4cc0bfd4c29485567ac8c95b529a93e3
sourishv <sourishvishwanatham@gmail.com>
2023-12-11 17:20:39 -0500

    added basketball, still copying dr0id tutorial

--- LeBron.py
+++ LeBron.py
@@ -12,11 +12,22 @@ def main():
     pygame.display.set_caption("minimal program")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((240,180))
+    screen = pygame.display.set_mode((800,600))
      
     # define a variable to control the main loop
     running = True
      
+    #load image
+    image = pygame.image.load("basketball.png")
+    #Colorkey makes pixels w same color not be drawn - white here
+    image.set_colorkey((255,255,255))
+    #transparency: 0 - 255, 0 is transparent, 255 is opaque; 128 special value, faster than other per-surface values
+    image.set_alpha(128)
+    #blit
+    screen.blit(image, (50, 50))
+    #update screen
+    pygame.display.flip()
+    
     # main loop
     while running:
         # event handling, gets all event from the event queue
